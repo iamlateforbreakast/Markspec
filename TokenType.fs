924224386308bce942b4ef11278901f5b8969bf0
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-11-03 17:50:25 +0000

    Add Lines type and enhance token handling

--- TokenType.fs
+++ TokenType.fs
@@ -1,5 +1,11 @@
 module TokenType
 
+    type Lines =
+        { Buffer : char list
+          Column : int
+          Line : int
+        }
+
     type TokenText =
         { Text : string
           Line : int }
@@ -7,12 +13,39 @@ module TokenType
     type TokenEOF =
         { Line : int }
 
+    type TokenEOL =
+        { Type : int 
+          Line : int }
+
     type Token =
         | Text of TokenText
         | EOF of TokenEOF
+        | EOL of TokenEOL
 
-    let getToken (content : char list)(line: int) : Token * char list =
-        match content with
-        | [] -> (EOF {Line = line}, [])
-        | ' ' :: rest -> (Text {Text = "Hello"; Line = line} , [])
-        | _ -> (Text {Text = "Hello"; Line = line} , [])
+    let isText (c : char) =
+        not ((c = '\r') || (c = '\n') || (c = ' ') || (c = '\t'))
+
+    let readTextToken (content : Lines) : Token  * Lines =
+        let rec loop(content : Lines)(text: char list) =
+            match content.Buffer with
+            | [] -> (content, text)
+            | c :: rest when isText c -> loop {content with Buffer = rest} (c :: text)
+            | _ -> (content, text)
+            //chars |> List.toArray |> (fun s -> System.String s)
+        let (a,b) = loop content []
+        (Text {Text = System.String(b |> List.toArray); Line = content.Line} , a)
+
+    let getToken (content : Lines) : Token * Lines =
+        match content.Buffer with
+        | [] -> (EOF {Line = content.Line}, { content with Buffer = [] })
+        | '\r' :: '\n' :: rest -> ((EOL {Type = 1; Line = content.Line}), { content with Buffer = rest}) // Windows EOL
+        | '\n' :: rest -> ((EOL {Type = 2; Line = content.Line}), { content with Buffer = rest}) // Linux EOL
+        | '\r' :: rest -> ((EOL {Type = 3; Line = content.Line}), { content with Buffer = rest}) // Mac EOL
+        | c :: rest when isText c -> readTextToken content
+        | _ -> (Text {Text = "Error"; Line = content.Line}, { content with Buffer = [] }) // Never executed
+
+    let printToken (token : Token) =
+        match token with
+        | Text t -> printfn "Token: Text { Text = \"%s\"; Line = %d }" t.Text t.Line
+        | EOF e -> printfn "Token: EOF { Line = %d }" e.Line
+        | EOL e -> printfn "Token: EOL { Type = %d }" e.Type
