574f1a4b0a19fb229d4ffd0a8edb1d435e2c3e92
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2025-11-03 17:51:36 +0000

    Refactor parseToken to handle lines instead of chars

--- ParseToken.fs
+++ ParseToken.fs
@@ -1,12 +1,16 @@
 module ParseToken
     open TokenType
 
+    let printToken (tokens : Token list) =
+        (tokens |> List.iter printToken)
+
     let parseToken (content : string) : Token list =
-        let rec loop (rest : char list) (tokens : Token list) =
-            let (newToken,rest) = getToken rest 0
+        let rec loop (rest : Lines) (tokens : Token list) : Token list =
+            let (newToken,rest) = getToken rest
             match newToken with
-            | TokenEOF -> TokenEOF :: tokens
-            | _ -> loop ()
-        let tokens = []
-        let (newToken,rest) = getToken (content |> Seq.toList) 0
-        newToken :: tokens
+            | EOF _ -> (newToken :: tokens)
+            | EOL _ -> loop {rest with Line = rest.Line + 1} tokens
+            | _ -> loop rest (newToken::tokens) 
+
+        let lines = {Buffer = (content |> Seq.toList); Line = 0; Column = 0}
+        loop lines [] |> List.rev
